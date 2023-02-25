f108d560950875d192e4dbbd9d3c2c234bd35936
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-25 23:56:31 +0600

    even odd numbers with while loop

---
+++ problem1.js
@@ -0,0 +1,20 @@
+// Display 1 to 10 numbers
+var number = 1;
+while(number <= 10){
+    // console.log(number);
+    number++;
+}
+
+// Display 1 to 10 even numbers
+var number2 = 2;
+while(number2 <= 10){
+    // console.log(number2);
+    number2 = number2 + 2;
+}
+
+// Display 1 to 10 odd numbers 
+var thirdNumber = 1;
+while(thirdNumber <= 10) {
+    console.log(thirdNumber);
+    thirdNumber = thirdNumber + 2;
+}
