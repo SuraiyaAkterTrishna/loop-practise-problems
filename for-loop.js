c2bd25a7c2e7e706e2cd7c2e65b637e6d518aad2
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-26 00:43:48 +0600

    for loop

---
+++ for-loop.js
@@ -0,0 +1,14 @@
+// Display 1 to 10 numbers
+for(var i = 1; i <= 10; i++){
+    // console.log(i);
+}
+
+// Display 1 to 10 even numbers
+for(var j = 2; j <= 10; j+=2) {
+    // console.log(j);
+}
+
+// Display 1 to 10 odd numbers 
+for(var x = 1; x <=10; x+=2){
+    console.log(x);
+}
