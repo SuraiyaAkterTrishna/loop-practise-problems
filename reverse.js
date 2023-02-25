190d69c4de7f711d8dbca06348bd32ff3d5f57dd
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-26 02:16:19 +0600

    reverse while and for loop

---
+++ reverse.js
@@ -0,0 +1,11 @@
+// while loop reverse
+var number = 10; 
+while(number >=1) {
+    // console.log(number);
+    number--;
+}
+
+// for loop reverse
+for(var i = 10; i >= 1; i--) {
+    console.log(i);
+} 
