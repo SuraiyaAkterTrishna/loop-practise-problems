e3dd5b143b67c73b22782086e2197ae7ca646b28
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-26 15:50:50 +0600

    practice problem 2 done

---
+++ problem2.js
@@ -0,0 +1,27 @@
+var myWork = ["unlock module at 8pm", "quickly watch the videos and practice", "take notes", "practice yourself", "if any confusion then join the support session"];
+
+// display with for loop
+for(var i = 0; i < myWork.length; i++) {
+    // console.log(myWork[i]);
+} 
+
+// while 
+var i = 0; 
+while(i < myWork.length) {
+    // console.log(myWork[i]);
+    i++;
+}
+
+// reverse for
+for(var i = myWork.length-1; i >= 0; i--){
+    // console.log(myWork[i]);
+}
+
+// reverse while 
+var i = myWork.length-1;
+while(i >= 0) {
+    console.log(myWork[i]);
+    i--;
+}
+
+
